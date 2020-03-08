ea9a0f6898494b7aa0841881850266aa48cd6d51
Felipe Benevides <felipe.benevides0@gmail.com>
2020-03-08 12:44:19 -0300

    Apagando para Organizar

--- Linguagem de Scripts/Lista_Atividades_01/ex01_media.js
+++
@@ -1,8 +0,0 @@
-let notas = [10,8,7]
-let soma = 0
-for(index in notas){
-    soma+=notas[index]
-}
-console.log(parseFloat((soma/notas.length).toFixed(2)))
-
-
--- Linguagem de Scripts/Lista_Atividades_01/ex03_maior_Numero.js
+++
@@ -1,6 +0,0 @@
-let numeros = [10,2,5,800000,14]
-let maior = 0
-for(index in numeros){
-    if(maior <= numeros[index]) maior = numeros[index]
-}
-console.log(maior)
