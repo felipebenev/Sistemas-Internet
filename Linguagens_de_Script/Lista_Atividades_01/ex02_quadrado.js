66feb272c77665caf4fe29254c0b4cca71aac1bb
Felipe Benevides <felipe.benevides0@gmail.com>
2020-03-08 13:59:12 -0300

    Delete ex02_quadrado.js

--- Linguagens_de_Script/Lista_Atividades_01/ex02_quadrado.js
+++
@@ -1,9 +0,0 @@
-let numeros = [10,20,30,40,50]
-function quadrado(numero){
-    let quadrado = numero**2
-    return console.log(`O quadrado de ${numero} é: ${quadrado}`)
-}
-for(index in numeros){
-    quadrado(numeros[index])
-}
-
